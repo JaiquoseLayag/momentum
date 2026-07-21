b78b16e0e8c67bdb25e78549e9a21b1863188976
Jacuzzi <jheklayag@gmail.com>
2026-07-21 11:36:10 +0800

    created the  task model and database relationship

---
+++ database/migrations/2026_07_21_032021_create_tasks_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->string('title');
+
+            $table->text('description')
+                  ->nullable();
+
+            $table->string('category');
+
+            $table->enum('priority', [
+                'low',
+                'medium',
+                'high'
+            ])->default('medium');
+
+            $table->enum('difficulty', [
+                'easy',
+                'medium',
+                'hard'
+            ])->default('medium');
+
+            $table->integer('estimated_time')
+                  ->nullable();
+
+            $table->date('deadline')
+                  ->nullable();
+
+            $table->enum('status', [
+                'pending',
+                'completed'
+            ])->default('pending');
+
+            $table->index(['status', 'priority']);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
